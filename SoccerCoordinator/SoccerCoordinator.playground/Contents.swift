7a338f6525416227e69ab3b562dd5343b59e7f05
Andreia Fonseca <almeidafonseca@icloud.com>
2017-12-18 20:44:00 +0100

    Implement planer distribution based on experience and height.

--- SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
@@ -1,79 +1,260 @@
-// Creating a Dictionary for players
+// Creating an Array of Dictionaries for players
 
 var players: [[String: String]] = [
-    ["Name": "Joe Smith", "Height": "42", "Experience": "Yes", "Guardian": "Jim and Jan Smith"],
-    ["Name": "Jill Tanner", "Height": "tirthy six", "Experience": "Yes", "Guardian": "Clara Tanner"],
-    ["Name": "Bill Bon", "Height": "forthy three", "Experience": "Yes", "Guardian": "Sara and Jenny Bon"],
-    ["Name": "Eva Gordon", "Height": "forthy five", "Experience": "No", "Guardian": "Wendy and Mike Gordon"],
-    ["Name": "Matt Gill", "Height": "forthy", "Experience": "No", "Guardian": "Charles and Sylvia Gill"],
-    ["Name": "Kimmy Stein", "Height": "forthy one", "Experience": "No", "Guardian": "Bill and Hillary Stein"],
-    ["Name": "Sammy Adams", "Height": "forthy five", "Experience": "No", "Guardian": "Jeff Adams"],
-    ["Name": "Karl Saygan", "Height": "forthy two", "Experience": "Yes", "Guardian": "Heather Bledsoe"],
-    ["Name": "Suzane Greenberg", "Height":" forthy four", "Experience": "Yes", "Guardian": "Henrietta Dumas"],
-    ["Name": "Sal Dali", "Height": "forthy one", "Experience": "No", "Guardian": "Gala Dali"],
-    ["Name": "Joe Kavalier", "Height": "tirthy nine", "Experience": "No", "Guardian": "Sam and Elaine Kavalier"],
-    ["Name": "Ben Finkelstein", "Height": "forthy four", "Experience": "No", "Guardian": "Aaron and Jill Finkelstein"],
-    ["Name": "Diego Soto", "Height": "forthy one", "Experience": "Yes", "Guardian": "Robin and Sarika Soto"],
-    ["Name": "Chloe Alaska", "Height": "forthy seven", "Experience": "No", "Guardian": "David and Jamie Alaska"],
-    ["Name": "Arnold Willis", "Height": "forthy three", "Experience": "No", "Guardian": "Claire Willis"],
-    ["Name": "Phillip Helm", "Height": "forthy four", "Experience": "Yes", "Guardian": "Thomas Helm and Eva Jones"],
-    ["Name": "Les Clay", "Height": "forthy two", "Experience": "Yes", "Guardian": "Wynonna Brown"],
-    ["Name": "Herschel Krustofski", "Height": "forthy five", "Experience": "Yes", "Guardian": "Hyman and Rachel Krustofski"]
+    ["name": "Joe Smith", "height": "42", "experience": "Yes", "guardian": "Jim and Jan Smith"],
+    ["name": "Jill Tanner", "height": "36", "experience": "Yes", "guardian": "Clara Tanner"],
+    ["name": "Bill Bon", "height": "43", "experience": "Yes", "guardian": "Sara and Jenny Bon"],
+    ["name": "Eva Gordon", "height": "45", "experience": "No", "guardian": "Wendy and Mike Gordon"],
+    ["name": "Matt Gill", "height": "40", "experience": "No", "guardian": "Charles and Sylvia Gill"],
+    ["name": "Kimmy Stein", "height": "41", "experience": "No", "guardian": "Bill and Hillary Stein"],
+    ["name": "Sammy Adams", "height": "45", "experience": "No", "guardian": "Jeff Adams"],
+    ["name": "Karl Saygan", "height": "42", "experience": "Yes", "guardian": "Heather Bledsoe"],
+    ["name": "Suzane Greenberg", "height": "44", "experience": "Yes", "guardian": "Henrietta Dumas"],
+    ["name": "Sal Dali", "height": "41", "experience": "No", "guardian": "Gala Dali"],
+    ["name": "Joe Kavalier", "height": "39", "experience": "No", "guardian": "Sam and Elaine Kavalier"],
+    ["name": "Ben Finkelstein", "height": "44", "experience": "No", "guardian": "Aaron and Jill Finkelstein"],
+    ["name": "Diego Soto", "height": "41", "experience": "Yes", "guardian": "Robin and Sarika Soto"],
+    ["name": "Chloe Alaska", "height": "47", "experience": "No", "guardian": "David and Jamie Alaska"],
+    ["name": "Arnold Willis", "height": "43", "experience": "No", "guardian": "Claire Willis"],
+    ["name": "Phillip Helm", "height": "44", "experience": "Yes", "guardian": "Thomas Helm and Eva Jones"],
+    ["name": "Les Clay", "height": "42", "experience": "Yes", "guardian": "Wynonna Brown"],
+    ["name": "Herschel Krustofski", "height": "45", "experience": "Yes", "guardian": "Hyman and Rachel Krustofski"]
 ]
 
-//Creating Teams
+// Creating Teams
 
-var teamSharks: [String] = []
-var teamDragons: [String] = []
-var teamRaptors: [String] = []
+var teamSharks: [[String: String]] = []
+var teamDragons: [[String: String]] = []
+var teamRaptors: [[String: String]] = []
 
-// Creating Letters
+// Distribute evenly players in the teams based on the experience and height
 
-var letters: [String]
+var teamSharksHeightTotal: Int = 0
+var teamDragonsHeightTotal: Int = 0
+var teamRaptorsHeightTotal: Int = 0
 
-// Distribue evenly players in the teams based on the experiencie
+// Players with experience, with height between 36 and 39
 
 var count = 0
+
 for var player in players {
-    if player ["Experience"] == "Yes" {
+    let height = Int(player["height"]!)!
+    
+    if player["experience"] == "Yes" && height >= 36 && height <= 39 {
         count += 1
         switch count % 3 {
         case 0:
-            player["Team"] = "Sharks"
-            teamSharks.append(player["Name"]!)
+            // Add the player's height to the total team's height
+            teamSharksHeightTotal += height
+            
+            teamSharks.append(player)
         case 1:
-            player["Team"] = "Dragons"
-            teamDragons.append(player["Name"]!)
+            // Add the player's height to the total team's height
+            teamDragonsHeightTotal += height
+            
+            teamDragons.append(player)
         case 2:
-            player["Team"] = "Raptors"
-            teamRaptors.append(player["Name"]!)
+            // Add the player's height to the total team's height
+            teamRaptorsHeightTotal += height
+            
+            teamRaptors.append(player)
         default:
             continue
         }
     }
 }
 
-// Letters
+// Players with experience, with height between 40 and 43
 
-for players in players {
-    let name = player["Name"]
-    let guardians = player["Guardians"]
-    let team = player["Team"]
-    var Scheduale = "March 17, 3pm"
-    if team == "Sharks" {
-        Scheduale = "March 17, 1pm"
-    } else if team == "Dragons"
-        Scheduale = "March 17, 1pm"
-    } else {
-        Scheduale = "March 18, 1pm"
+count = 0
+for var player in players {
+    let height = Int(player["height"]!)!
+    
+    if player["experience"] == "Yes" && height >= 40 && height <= 43 {
+        count += 1
+        switch count % 3 {
+        case 0:
+            // Add the player's height to the total team's height
+            teamSharksHeightTotal += height
+            
+            teamSharks.append(player)
+        case 1:
+            // Add the player's height to the total team's height
+            teamDragonsHeightTotal += height
+            
+            teamDragons.append(player)
+        case 2:
+            // Add the player's height to the total team's height
+            teamRaptorsHeightTotal += height
+            
+            teamRaptors.append(player)
+        default:
+            continue
+        }
     }
-let letter = "Dear Mr./Mrs. \(Guardian) by this mean I want to tell you that your son \(player) was placed in the team \(Team) and you should attend to their first team team practice in \(Scheduale). Sincerelly, Soccer League Coordinator A. Fonseca"
+}
+
+// Players with experience, with height between 44 and 47
+
+count = 0
+for var player in players {
+    let height = Int(player["height"]!)!
+    
+    if player["experience"] == "Yes" && height >= 44 && height <= 47 {
+        count += 1
+        switch count % 3 {
+        case 0:
+            // Add the player's height to the total team's height
+            teamSharksHeightTotal += height
+            
+            teamSharks.append(player)
+        case 1:
+            // Add the player's height to the total team's height
+            teamDragonsHeightTotal += height
+            
+            teamDragons.append(player)
+        case 2:
+            // Add the player's height to the total team's height
+            teamRaptorsHeightTotal += height
+            
+            teamRaptors.append(player)
+        default:
+            continue
+        }
+    }
+}
+
+// Players without experience, with height between 36 and 39
+
+count = 0
+for var player in players {
+    let height = Int(player["height"]!)!
+    
+    if player["experience"] == "No" && height >= 36 && height <= 39 {
+        count += 1
+        switch count % 3 {
+        case 0:
+            // Add the player's height to the total team's height
+            teamSharksHeightTotal += height
+            
+            teamSharks.append(player)
+        case 1:
+            // Add the player's height to the total team's height
+            teamDragonsHeightTotal += height
+            
+            teamDragons.append(player)
+        case 2:
+            // Add the player's height to the total team's height
+            teamRaptorsHeightTotal += height
+            
+            teamRaptors.append(player)
+        default:
+            continue
+        }
+    }
+}
+
+// Players without experience, with height between 40 and 43
+
+count = 0
+for var player in players {
+    let height = Int(player["height"]!)!
+    
+    if player["experience"] == "No" && height >= 40 && height <= 43 {
+        count += 1
+        switch count % 3 {
+        case 0:
+            // Add the player's height to the total team's height
+            teamSharksHeightTotal += height
+            
+            teamSharks.append(player)
+        case 1:
+            // Add the player's height to the total team's height
+            teamDragonsHeightTotal += height
+            
+            teamDragons.append(player)
+        case 2:
+            // Add the player's height to the total team's height
+            teamRaptorsHeightTotal += height
+            
+            teamRaptors.append(player)
+        default:
+            continue
+        }
+    }
+}
+
+// Players without experience, with height between 44 and 47
+
+count = 0
+for var player in players {
+    let height = Int(player["height"]!)!
+    
+    if player["experience"] == "No" && height >= 44 && height <= 47 {
+        count += 1
+        switch count % 3 {
+        case 0:
+            // Add the player's height to the total team's height
+            teamSharksHeightTotal += height
+            
+            teamSharks.append(player)
+        case 1:
+            // Add the player's height to the total team's height
+            teamDragonsHeightTotal += height
+            
+            teamDragons.append(player)
+        case 2:
+            // Add the player's height to the total team's height
+            teamRaptorsHeightTotal += height
+            
+            teamRaptors.append(player)
+        default:
+            continue
+        }
+    }
+}
+
+// Creating Letters
+
+var letters: [String] = []
+
+for player in teamDragons {
+    let name = player["name"]!
+    let guardian = player["guardian"]!
+    let schedule = "March 17, 1pm"
+    
+    let letter = "Dear Mr./Mrs. \(guardian) by this mean I want to tell you that your son \(name) was placed in the team Dragons and you should attend to their first team team practice in \(schedule). Sincerelly, Soccer League Coordinator A. Fonseca"
     letters.append(letter)
 }
 
-//print
-
-print (letters)
-for letter in letters {
-    print(letters)
+for player in teamSharks {
+    let name = player["name"]!
+    let guardian = player["guardian"]!
+    let schedule = "March 17, 3pm"
+    
+    let letter = "Dear Mr./Mrs. \(guardian) by this mean I want to tell you that your son \(name) was placed in the team Sharks and you should attend to their first team team practice in \(schedule). Sincerelly, Soccer League Coordinator A. Fonseca"
+    letters.append(letter)
 }
+
+for player in teamRaptors {
+    let name = player["name"]!
+    let guardian = player["guardian"]!
+    let schedule = "March 18, 1pm"
+    
+    let letter = "Dear Mr./Mrs. \(guardian) by this mean I want to tell you that your son \(name) was placed in the team Raptors and you should attend to their first team team practice in \(schedule). Sincerelly, Soccer League Coordinator A. Fonseca"
+    letters.append(letter)
+}
+
+// Printing letters
+
+for letter in letters {
+    print(letter)
+}
+
+// Print team's average height
+
+print("Sharks team average height: \(Float(teamSharksHeightTotal) / Float(teamSharks.count))")
+print("Dragons team average height: \(Float(teamDragonsHeightTotal) / Float(teamDragons.count))")
+print("Raptors team average height: \(Float(teamRaptorsHeightTotal) / Float(teamRaptors.count))")
+
+
